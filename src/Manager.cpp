3ea41f81290bd07950809164723b8ef436f0fbb3
Derek Christ <christ.derek@gmail.com>
2022-11-21 11:34:47 +0100

    Add implementation for getTopVarName

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -107,7 +107,7 @@ BDD_ID Manager::xnor2(BDD_ID a, BDD_ID b)
 
 std::string Manager::getTopVarName(const BDD_ID &root)
 {
-    return {};
+    return Tabel.at(topVar(root)).Label;
 }
 
 void Manager::findNodes(const BDD_ID &root, std::set<BDD_ID> &nodes_of_root)
