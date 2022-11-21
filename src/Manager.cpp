cedb311d34bd910eef50e2d8f3270ae6d5d1d4ca
Derek Christ <christ.derek@gmail.com>
2022-11-21 09:20:26 +0100

    Add dummy implementations for ite, and2, or2
    Add dummy implementations so that unrelated tests
    to coFactorFalse do not segfault.

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -42,6 +42,7 @@ BDD_ID Manager::topVar(BDD_ID f)
 
 BDD_ID Manager::ite(BDD_ID i, BDD_ID t, BDD_ID e)
 {
+    return 0;
 }
 
 BDD_ID Manager::coFactorTrue(BDD_ID f, BDD_ID x)
@@ -80,10 +81,12 @@ BDD_ID Manager::neg(BDD_ID a)
 
 BDD_ID Manager::and2(BDD_ID a, BDD_ID b)
 {
+    return 0;
 }
 
 BDD_ID Manager::or2(BDD_ID a, BDD_ID b)
 {
+    return 0;
 }
 
 BDD_ID Manager::xor2(BDD_ID a, BDD_ID b)
